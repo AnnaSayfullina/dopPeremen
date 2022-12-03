9114a4c3ce2bc78ee2a97057ee7324283bc5aeec
anna <anna@sayf.su>
2022-12-03 23:35:22 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 47;
+        int b = 13;
+        int c = a / b;
+        int d = a % b;
+        System.out.println(c + " и " + d +" остаток");
     }
 }
